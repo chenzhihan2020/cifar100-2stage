5909063499cdaa2be7f5e0862fad67c3ad6a9397
chenzhihan2020 <39210258+chenzhihan2020@users.noreply.github.com>
2019-12-06 15:55:54 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,125 +15,125 @@ from torch.utils.data import DataLoader
 
 #from dataset import CIFAR100Train, CIFAR100Test
 
-def get_network(args, use_gpu=True):
+def get_network(name, use_gpu=True):
     """ return given network
     """
 
-    if args.net == 'vgg16':
+    if name == 'vgg16':
         from models.vgg import vgg16_bn
         net = vgg16_bn()
-    elif args.net == 'vgg13':
+    elif name == 'vgg13':
         from models.vgg import vgg13_bn
         net = vgg13_bn()
-    elif args.net == 'vgg11':
+    elif name == 'vgg11':
         from models.vgg import vgg11_bn
         net = vgg11_bn()
-    elif args.net == 'vgg19':
+    elif name == 'vgg19':
         from models.vgg import vgg19_bn
         net = vgg19_bn()
-    elif args.net == 'densenet121':
+    elif name == 'densenet121':
         from models.densenet import densenet121
         net = densenet121()
-    elif args.net == 'densenet161':
+    elif name == 'densenet161':
         from models.densenet import densenet161
         net = densenet161()
-    elif args.net == 'densenet169':
+    elif name == 'densenet169':
         from models.densenet import densenet169
         net = densenet169()
-    elif args.net == 'densenet201':
+    elif name == 'densenet201':
         from models.densenet import densenet201
         net = densenet201()
-    elif args.net == 'googlenet':
+    elif name == 'googlenet':
         from models.googlenet import googlenet
         net = googlenet()
-    elif args.net == 'inceptionv3':
+    elif name == 'inceptionv3':
         from models.inceptionv3 import inceptionv3
         net = inceptionv3()
-    elif args.net == 'inceptionv4':
+    elif name == 'inceptionv4':
         from models.inceptionv4 import inceptionv4
         net = inceptionv4()
-    elif args.net == 'inceptionresnetv2':
+    elif name == 'inceptionresnetv2':
         from models.inceptionv4 import inception_resnet_v2
         net = inception_resnet_v2()
-    elif args.net == 'xception':
+    elif name == 'xception':
         from models.xception import xception
         net = xception()
-    elif args.net == 'resnet18':
+    elif name == 'resnet18':
         from models.resnet import resnet18
         net = resnet18()
-    elif args.net == 'resnet34':
+    elif name == 'resnet34':
         from models.resnet import resnet34
         net = resnet34()
-    elif args.net == 'resnet50':
+    elif name == 'resnet50':
         from models.resnet import resnet50
         net = resnet50()
-    elif args.net == 'resnet101':
+    elif name == 'resnet101':
         from models.resnet import resnet101
         net = resnet101()
-    elif args.net == 'resnet152':
+    elif name == 'resnet152':
         from models.resnet import resnet152
         net = resnet152()
-    elif args.net == 'preactresnet18':
+    elif name == 'preactresnet18':
         from models.preactresnet import preactresnet18
         net = preactresnet18()
-    elif args.net == 'preactresnet34':
+    elif name == 'preactresnet34':
         from models.preactresnet import preactresnet34
         net = preactresnet34()
-    elif args.net == 'preactresnet50':
+    elif name == 'preactresnet50':
         from models.preactresnet import preactresnet50
         net = preactresnet50()
-    elif args.net == 'preactresnet101':
+    elif name == 'preactresnet101':
         from models.preactresnet import preactresnet101
         net = preactresnet101()
-    elif args.net == 'preactresnet152':
+    elif name == 'preactresnet152':
         from models.preactresnet import preactresnet152
         net = preactresnet152()
-    elif args.net == 'resnext50':
+    elif name == 'resnext50':
         from models.resnext import resnext50
         net = resnext50()
-    elif args.net == 'resnext101':
+    elif name == 'resnext101':
         from models.resnext import resnext101
         net = resnext101()
-    elif args.net == 'resnext152':
+    elif name == 'resnext152':
         from models.resnext import resnext152
         net = resnext152()
-    elif args.net == 'shufflenet':
+    elif name == 'shufflenet':
         from models.shufflenet import shufflenet
         net = shufflenet()
-    elif args.net == 'shufflenetv2':
+    elif name == 'shufflenetv2':
         from models.shufflenetv2 import shufflenetv2
         net = shufflenetv2()
-    elif args.net == 'squeezenet':
+    elif name == 'squeezenet':
         from models.squeezenet import squeezenet
         net = squeezenet()
-    elif args.net == 'mobilenet':
+    elif name == 'mobilenet':
         from models.mobilenet import mobilenet
         net = mobilenet()
-    elif args.net == 'mobilenetv2':
+    elif name == 'mobilenetv2':
         from models.mobilenetv2 import mobilenetv2
         net = mobilenetv2()
-    elif args.net == 'nasnet':
+    elif name == 'nasnet':
         from models.nasnet import nasnet
         net = nasnet()
-    elif args.net == 'attention56':
+    elif name == 'attention56':
         from models.attention import attention56
         net = attention56()
-    elif args.net == 'attention92':
+    elif name == 'attention92':
         from models.attention import attention92
         net = attention92()
-    elif args.net == 'seresnet18':
+    elif name == 'seresnet18':
         from models.senet import seresnet18
         net = seresnet18()
-    elif args.net == 'seresnet34':
+    elif name == 'seresnet34':
         from models.senet import seresnet34 
         net = seresnet34()
-    elif args.net == 'seresnet50':
+    elif name == 'seresnet50':
         from models.senet import seresnet50 
         net = seresnet50()
-    elif args.net == 'seresnet101':
+    elif name == 'seresnet101':
         from models.senet import seresnet101 
         net = seresnet101()
-    elif args.net == 'seresnet152':
+    elif name == 'seresnet152':
         from models.senet import seresnet152
         net = seresnet152()
 
