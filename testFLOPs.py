1e188befb640e1bfb2d9c5b23cee2baae06afe2a
ShuYang <shuyang1995@ucsb.edu>
2019-12-06 22:52:44 -0800

    change format

--- testFLOPs.py
+++ testFLOPs.py
@@ -5,4 +5,4 @@ from FLOPs import get_model_complexity_info
 net = squeezenet()
 flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=False)
 print("FLOPs: {}".format(flops))
-print("Params: {}" + params)
+print("Params: {}".format(params))
