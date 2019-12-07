efa88c8dfe95573e136a9613d56903e241881b2c
ShuYang <shuyang1995@ucsb.edu>
2019-12-06 17:30:28 -0800

    have a try

--- testFLOPs.py
+++ testFLOPs.py
@@ -1,8 +1,8 @@
-from models.mobilenetv2 import mobilenetv2
+from models.squeezenet import squeezenet
 from FLOPs import get_model_complexity_info
 
 
-net = mobilenetv2()
+net = squeezenet()
 flops, params = get_model_complexity_info(net,(32,32),as_strings=True,print_per_layer_stat=True)
 print("FLOPs: {}".format(flops))
 print("Params: {}" + params)
