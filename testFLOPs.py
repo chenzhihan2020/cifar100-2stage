1edf10de4abc1e5109e5bb30844f6abd66af2a70
ShuYang <shuyang1995@ucsb.edu>
2019-12-06 17:10:26 -0800

    fix a typo

--- testFLOPs.py
+++ testFLOPs.py
@@ -3,6 +3,6 @@ from FLOPs import get_model_complexity_info
 
 
 net = mobilenetv2
-flops, params = get_model_complexity_info(net,(32,32),as_strings=True,print_per_laryer_stat=True)
+flops, params = get_model_complexity_info(net,(32,32),as_strings=True,print_per_layer_stat=True)
 print("FLOPs: {}".format(flops))
 print("Params: {}" + params)
