f1da9996a7f979fc21b87265c607eae086b2a80f
ShuYang <shuyang1995@ucsb.edu>
2019-12-06 22:39:44 -0800

    have a try

--- testFLOPs.py
+++ testFLOPs.py
@@ -3,6 +3,6 @@ from FLOPs import get_model_complexity_info
 
 
 net = squeezenet()
-flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=True)
+flops, params = get_model_complexity_info(net,(3,32,32),as_strings=False,print_per_layer_stat=True)
 print("FLOPs: {}".format(flops))
 print("Params: {}" + params)
