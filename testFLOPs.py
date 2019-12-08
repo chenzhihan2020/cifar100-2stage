6f05b8e021d7c3aab168606ccfa572cf77cb3164
ShuYang <shuyang1995@ucsb.edu>
2019-12-08 11:51:41 -0800

    vgg19_bn

--- testFLOPs.py
+++ testFLOPs.py
@@ -1,8 +1,8 @@
-from models.senet import seresnet18
+from models.vgg import vgg19_bn
 from FLOPs import get_model_complexity_info
 
 
-net = seresnet18()
+net = vgg19_bn()
 flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=False)
 print("FLOPs: {}".format(flops))
 print("Params: {}".format(params))
