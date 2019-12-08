8ef2ccd4ddcc5a77db7f509eb106a6761e72f1b7
ShuYang <shuyang1995@ucsb.edu>
2019-12-08 11:30:25 -0800

    preactresnet152

--- testFLOPs.py
+++ testFLOPs.py
@@ -1,8 +1,8 @@
-from models.resnet import resnet18
+from models.preactresnet import preactresnet152
 from FLOPs import get_model_complexity_info
 
 
-net = resnet18()
+net = preactresnet152()
 flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=False)
 print("FLOPs: {}".format(flops))
 print("Params: {}".format(params))
