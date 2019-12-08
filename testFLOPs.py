5274eff35ccd6a02e0988328508cb9879351cd2a
ShuYang <shuyang1995@ucsb.edu>
2019-12-08 11:25:06 -0800

    resnet18

--- testFLOPs.py
+++ testFLOPs.py
@@ -1,8 +1,8 @@
-from models.squeezenet import squeezenet
+from models.resnet import resnet18
 from FLOPs import get_model_complexity_info
 
 
-net = squeezenet()
+net = resnet18()
 flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=False)
 print("FLOPs: {}".format(flops))
 print("Params: {}".format(params))
