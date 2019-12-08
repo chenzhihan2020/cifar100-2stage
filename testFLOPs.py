d0e8f39b54fda70762fa9de26dc53145b75a7442
ShuYang <shuyang1995@ucsb.edu>
2019-12-08 11:31:35 -0800

    101

--- testFLOPs.py
+++ testFLOPs.py
@@ -1,8 +1,8 @@
-from models.preactresnet import preactresnet152
+from models.preactresnet import preactresnet101
 from FLOPs import get_model_complexity_info
 
 
-net = preactresnet152()
+net = preactresnet101()
 flops, params = get_model_complexity_info(net,(3,32,32),as_strings=True,print_per_layer_stat=False)
 print("FLOPs: {}".format(flops))
 print("Params: {}".format(params))
